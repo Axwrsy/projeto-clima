af3cd1ed52bac3a1684e2567eaa52643680fc5e4
carol <annasantos4530@gmail.com>
2025-06-03 16:54:45 -0300

    Add files via upload

---
+++ projeto-automação/main.py
@@ -0,0 +1,31 @@
+import requests
+
+def obter_clima(cidade, chave_api):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={chave_api}&units=metric&lang=pt_br"
+    resposta = requests.get(url)
+    
+    print(f"Status code: {resposta.status_code}")
+    print(f"Resposta: {resposta.text}")
+
+    if resposta.status_code == 200:
+        dados = resposta.json()
+        temp = dados["main"]["temp"]
+        sensacao = dados["main"]["feels_like"]
+        descricao = dados["weather"][0]["description"]
+        print(f"\n clima em {cidade.title()}:")
+        print(f"temperatura: {temp}°C")
+        print(f"sensação térmica: {sensacao}°C")
+        print(f"condição: {descricao}")
+
+        # Exemplo de automação simples
+        if temp > 30:
+            print(" alerta: está muito quente, ative o resfriamento!")
+        elif "chuva" in descricao:
+            print(" cuidado: está chovendo, verifique coberturas!")
+    else:
+        print(" erro ao obter dados.")
+
+# ---- CONFIG ----
+cidade = input("digite o nome da cidade: ")
+chave = "d5fbf89bdc9b3504227be6ab8446c56d"  # api
+obter_clima(cidade, chave)
